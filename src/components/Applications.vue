804928573138dd2c027700409231fba9eebcafba
Nathan Bland <nathan.bland@gmail.com>
2016-08-02 15:15:08 -0600

    Background is now a user setting, and unset by default

--- src/components/Applications.vue
+++ src/components/Applications.vue
@@ -3,12 +3,14 @@ import calculator from './apps/calculator.vue'
 import terminal from './apps/terminal/terminal.vue'
 import textEditor from './apps/textEditor.vue'
 import weather from './apps/weather.vue'
+import settings from './apps/settings.vue'
 export default {
   comps: {
     calculator,
     terminal,
     textEditor,
-    weather
+    weather,
+    settings
   }
 }
 </script>
